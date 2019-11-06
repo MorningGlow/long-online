55d50b642d93491ba8dd3ece8077cdd7d099768f
Lion Hua(LUXSHARE-ICT) <12755167@luxshare.com.cn>
2019-11-06 15:39:38 +0800

    Initial commit

---
+++ long-online-api/src/main/java/com/luxshare/OnlineApplication.java
@@ -0,0 +1,17 @@
+package com.luxshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 电商项目
+ *
+ * @author lion hua
+ * @since 2019-11-06
+ */
+@SpringBootApplication
+public class OnlineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineApplication.class, args);
+    }
+}
