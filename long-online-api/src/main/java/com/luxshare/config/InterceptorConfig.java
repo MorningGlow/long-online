3717d07a9dc3202cc6ddfbda28f17e96b0535ba2
lion hua <632105841@qq.com>
2019-11-26 15:23:18 +0800

    sql 性能分析

--- long-online-api/src/main/java/com/luxshare/config/InterceptorConfig.java
+++ long-online-api/src/main/java/com/luxshare/config/InterceptorConfig.java
@@ -1,16 +1,9 @@
 package com.luxshare.config;
 
-import com.luxshare.interceptors.CompleteSqlPrintInterceptor;
-import org.apache.ibatis.plugin.Interceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InterceptorConfig {
 
-//    @Bean
-//    public Interceptor getInterceptor(){
-//        return new CompleteSqlPrintInterceptor();
-//    }
 
 }
